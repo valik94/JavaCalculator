db3de11a37fd123a739b43920c895f841fd2e96c
Valentin <valentin.lyash@gmail.com>
2022-11-22 23:51:31 -0500

    had to redo the entire MainActivity.java implementation of the advanced/history button, clear button and the logic behind the calculation mode to ensure desired functionality with single digit computations works on the calculator.

--- app/src/main/java/com/example/ywca_f22_week1/CalculatorClass.java
+++ app/src/main/java/com/example/ywca_f22_week1/CalculatorClass.java
@@ -6,81 +6,95 @@ import java.util.ArrayList;
 public class CalculatorClass {
 
     //String record for history user's actions
-    public static String history = "";
+//    public static String history = "";
+    ArrayList<String> inputString = new ArrayList<String>();
     //ArrayList to store String history as elements of ArrayList
     public static ArrayList<String> historyArrayList = new ArrayList<>();
     //String for history list for advanced version
     public static String historyList = "";
 
-    public static boolean isNumber(String s){
-        switch (s){
-            case "0":
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-                return true;
-        }
-        return false;
+    int index = 0;
+    int result = 0;
+    int solution = 0;
+    int hisArrayIndex = 0;
+    String expression;
+    String expressionHist;
+
+
+
+//    public static boolean isNumber(String s){
+//        switch (s){
+//            case "0":
+//            case "1":
+//            case "2":
+//            case "3":
+//            case "4":
+//            case "5":
+//            case "6":
+//            case "7":
+//            case "8":
+//            case "9":
+//                return true;
+//        }
+//        return false;
+//    }
+//
+//    public static boolean isOperator(String s){
+//        switch(s){
+//            case "+":
+//            case "-":
+//            case "/":
+//            case "*":
+//                return true;
+//        }
+//        return false;
+//    }
+
+
+    public void push(String symbol) { //adds all users values in 1 list including operands and operators
+//        if (symbol.equals("=")){
+//            historyArrayList.clear();
+//            history+=symbol;
+//            parseStringToArrayList(history);
+//        } else {
+//            history += symbol;
+//        }
+        inputString.add(index, symbol);
+        index++;
+        expression = String.join("", inputString); // inputString contains: 1 + 2 - 1
+
     }
 
-    public static boolean isOperator(String s){
-        switch(s){
-            case "+":
-            case "-":
-            case "/":
-            case "*":
-                return true;
-        }
-        return false;
-    }
+//    public static void parseStringToArrayList (String s){
+//        String number = "";
+//        String symbol;
+//        for (int i = 0; i < s.length(); i++){
+//            symbol = "" + s.charAt(i);
+//            if (isNumber(symbol)){
+//                number += symbol;
+//            } else{
+//                historyArrayList.add(number);
+//                historyArrayList.add(symbol);
+//                number = "";
+//            }
+//        }
+//    }
+
+//    public static void advancedCalculate(){
+//        double result = Double.parseDouble(historyArrayList.get(0));
+//        double operand;
+//        String operator;
+//        for (int i=0; i<historyArrayList.size()-2; i =i+2) {
+//            operator = historyArrayList.get(i+1);
+//            operand = Double.parseDouble(historyArrayList.get(i+2));
+//            result = calculate(result, operand, operator);
+//        }
+//        history = history + result;
+//    }
 
 
-    public static void push(String symbol) {//adds all users values in 1 list including operands and operators
-        if (symbol.equals("=")){
-            historyArrayList.clear();
-            history+=symbol;
-            parseStringToArrayList(history);
-        } else {
-            history += symbol;
-        }
-    }
-
-    public static void parseStringToArrayList (String s){
-        String number = "";
-        String symbol;
-        for (int i = 0; i < s.length(); i++){
-            symbol = "" + s.charAt(i);
-            if (isNumber(symbol)){
-                number += symbol;
-            } else{
-                historyArrayList.add(number);
-                historyArrayList.add(symbol);
-                number = "";
-            }
-        }
-    }
-
-    public static void advancedCalculate(){
-        double result = Double.parseDouble(historyArrayList.get(0));
-        double operand;
-        String operator;
-        for (int i=0; i<historyArrayList.size()-2; i =i+2) {
-            operator = historyArrayList.get(i+1);
-            operand = Double.parseDouble(historyArrayList.get(i+2));
-            result = calculate(result, operand, operator);
-        }
-        history = history + result;
-    }
-
-
-    public static double calculate(double n1, double n2, String op){ //parses the string list and calculates the result
-        double result = 0;
+    public int calc(int n1, int n2, String op){ //parses the string list and calculates the result
+//        int result = 0;
         switch (op){
             case "+":
                 result = n1 + n2;
@@ -100,6 +114,43 @@ public class CalculatorClass {
         };
 //        history = n1 + " " + op + " " + n2 + " = " + result;
         return result;
+    }
+
+
+//Function calculates the result of the expression
+    public int calculate (){
+        // 1 + 2 - 1, size()=5
+        for (int i = 0; i<inputString.size(); i++){
+            System.out.println("inputStirng is:"+ inputString);
+            if ((inputString.get(i).equals("-")) ||
+                    (inputString.get(i).equals("+")) ||
+                    (inputString.get(i).equals("/")) ||
+                    (inputString.get(i).equals("*")))
+            {
+                String op = inputString.get(i);
+                int num1;
+                //for iteration 1, num1 is first number in the expression entered,
+                // for the rest of the iteration, num1 will is the result of previous calculations
+
+                if (i==1){
+                    num1 = Integer.parseInt(inputString.get(i - 1));
+                }
+                else { num1= solution; }
+                int num2 = Integer.parseInt(inputString.get(i + 1));
+                solution = calc(num1,num2,op);
+                System.out.println("solution is:"+ solution);
+
+            }
+        }
+        return solution;
+    }
+
+//function stores calculation history, called when calculator is in advanced mode (mode=1)
+    public void storeHistory(){
+    expressionHist = expression + "=" + solution; //storing string
+        System.out.println("object solution is:"+ solution);
+        historyArrayList.add(hisArrayIndex, expressionHist+"\n"); //arraylist of strings of history
+        hisArrayIndex++;
     }
 
 }
--- app/src/main/java/com/example/ywca_f22_week1/MainActivity.java
+++ app/src/main/java/com/example/ywca_f22_week1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ywca_f22_week1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,13 +15,13 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity
-    implements View.OnClickListener
-{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 // MVC: M = model, V= layout, C= Activity.
     /// MVVM = model, view, view model
+    CalculatorClass obj = new CalculatorClass();
+    int mode = 0;
 
-    CalculatorClass calculatorClass;
+//    CalculatorClass calculatorClass;
     Button num0;
     Button num1;
     Button num2;
@@ -34,49 +35,50 @@ public class MainActivity extends AppCompatActivity
 
     Button clear;
     Button equals;
-    Button add_but ;
+    Button add_but;
     Button sub_but;
-    Button times_but ;
+    Button times_but;
     Button div_but;
     TextView result_text;
     Button advancedBut;
+    TextView history_tv;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main_constraints);
         setContentView(R.layout.activity_main);
         //R.java is auto generated file, has all res ids (int)
-        Log.d("Calculator App" , "Activity Created");
+        Log.d("Calculator App", "Activity Created");
 
-       // layout = findViewById(R.id.rootlayout);
-        //layout.setBackgroundColor(getResources().getColor(R.color.DeepPink));
 
-        // this line is not correct
-        calculatorClass = new CalculatorClass();
-    //connecting activity_main.xml button ids to objects here
-         result_text = findViewById(R.id.resulttext);
-         num0 = findViewById(R.id.zero);
-         num1 = findViewById(R.id.one);
-         num2 = findViewById(R.id.two);
-         num3 = findViewById(R.id.three);
-         num4 = findViewById(R.id.four);
-         num5 = findViewById(R.id.five);
-         num6 = findViewById(R.id.six);
-         num7 = findViewById(R.id.seven);
-         num8 = findViewById(R.id.eight);
-         num9 = findViewById(R.id.nine);
+        // this line is not correct but ok for now
+//        calculatorClass = new CalculatorClass();
+        //connecting activity_main.xml button ids to objects here
+        result_text = findViewById(R.id.resulttext);
+        num0 = findViewById(R.id.zero);
+        num1 = findViewById(R.id.one);
+        num2 = findViewById(R.id.two);
+        num3 = findViewById(R.id.three);
+        num4 = findViewById(R.id.four);
+        num5 = findViewById(R.id.five);
+        num6 = findViewById(R.id.six);
+        num7 = findViewById(R.id.seven);
+        num8 = findViewById(R.id.eight);
+        num9 = findViewById(R.id.nine);
 
-         clear = findViewById(R.id.clear_but);
-         equals = findViewById(R.id.equals);
-         add_but = findViewById(R.id.add_but);
-         sub_but = findViewById(R.id.sub_but);
-         times_but = findViewById(R.id.multiply_but);
-         div_but = findViewById(R.id.div_but);
-        advancedBut = findViewById(R.id.toAdvancedButton);
+        clear = findViewById(R.id.clear_but);
+        equals = findViewById(R.id.equals);
+        add_but = findViewById(R.id.add_but);
+        sub_but = findViewById(R.id.sub_but);
+        times_but = findViewById(R.id.multiply_but);
+        div_but = findViewById(R.id.div_but);
+        advancedBut = findViewById(R.id.toAdvancedButton); //history
+        history_tv = findViewById(R.id.history_tv); //calcHistory
 
-         //setOnclickListener to activate action later such as onClick
+        //setOnclickListener to activate action later such as onClick
         num0.setOnClickListener(this);
         num1.setOnClickListener(this);
         num2.setOnClickListener(this);
@@ -92,83 +94,148 @@ public class MainActivity extends AppCompatActivity
         sub_but.setOnClickListener(this);
         times_but.setOnClickListener(this);
         div_but.setOnClickListener(this);
-        clear.setOnClickListener(this);
-        advancedBut.setOnClickListener(this);
+        equals.setOnClickListener(this);
 
+
+        //History button
+        advancedBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //advanced mode
+                if (mode ==0){
+                    mode =1;
+                    System.out.println("AFTER ENTERING ADVACNED MODE"+obj.inputString);
+                    history_tv.setVisibility(View.VISIBLE);
+                    advancedBut.setText("STANDARD - NO HISTORY");
+                    history_tv.setText("");
+                }
+                else {
+                    //standard mode no history
+                    mode =0;
+                    history_tv.setVisibility(View.INVISIBLE);
+                    advancedBut.setText("ADVANCED - WITH HISTORY");
+                    obj.historyArrayList.clear();
+                    obj.inputString.clear();
+
+                    obj.hisArrayIndex =0;
+                    obj.expression ="";
+                    obj.expressionHist = "";
+                }
+            }
+
+        });
+
+
+    // clear button
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result_text.setText("0");
+                obj.inputString.clear();
+                obj.index = 0;
+                obj.expression = "";
+            }
+
+        });
     }
+
+
 
     @Override
     public void onClick(View view) {
-        String symbol;
-        switch (view.getId()){
-            case R.id.clear:
-                CalculatorClass.history =""; //assign history to blank string ""
-                result_text.setText(CalculatorClass.history); // result_text displayed set to CalculatorClass.history which is ""
-                CalculatorClass.historyArrayList.clear();// clear the historyArrayList of strings
-                break;
-            case R.id.toAdvancedButton:
-                Intent firstIntent = new Intent(MainActivity.this, AdvancedVersion.class);
-                startActivity(firstIntent); //goes into AdvanceVersion
-                break;
-            default: //all other buttons for calculation (without clear button and advnacebutton
-                //get user input and validate it
-                symbol = ((Button) view).getText().toString();
-                if (CalculatorClass.isNumber(symbol)) {
-                    CalculatorClass.push(symbol);
-                    result_text.setText(CalculatorClass.history);
-                } else{
-                    if (CalculatorClass.history.isEmpty()){
-                        Toast.makeText(MainActivity.this, "First input has to be a number", Toast.LENGTH_SHORT).show();
-                    } else{
-                    String previousSymbol = String.valueOf(calculatorClass.history.charAt(CalculatorClass.history.length()-1));
-                    if (CalculatorClass.isOperator(previousSymbol)) {
-                       Toast.makeText(MainActivity.this, "You cannot use two operators in a row, enter a number", Toast.LENGTH_SHORT).show();
-                    } else {
-                       CalculatorClass.push(symbol);
-                       if (symbol.equals("=")){
-                           if (CalculatorClass.historyArrayList.size() < 3){
-                                Toast.makeText(MainActivity.this, "This cannot be calculated, please enter proper values and operators to calculate.", Toast.LENGTH_SHORT).show();
-                                CalculatorClass.historyArrayList.clear();
-                                calculatorClass.history = CalculatorClass.history.substring(0, CalculatorClass.history.length()-1 );
-                           } else {
+        Button button = (Button) view; // ?
+        String buttonText = button.getText().toString(); //storing the text of the button
 
-                           }
-                       }
-                    }
-                    }
-                }
+        if (view == equals) {
+            if (mode == 0) {
+                int result = obj.calculate(); //returns solution integer
+                result_text.setText(obj.expression + "=" + obj.solution); //display exp = solution
+            }
 
-                if (!calculatorClass.history.isEmpty())
-                    result_text.setText(calculatorClass.history);
-                else
-                    result_text.setText("No previous operator");
-                break;
-            default:
-                String op =  ((Button)view).getText().toString();
-                validate(op);
-        };
+            //ADVANCED mode
+            if (mode == 1) {
+                obj.storeHistory();
+                history_tv.setText(obj.historyArrayList.toString() //obj.historyArrayList.toString()
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(",", ""));
+
+            }
+        }else { //get the text of the button pressed
+            String data = ((Button) view).getText().toString();
+            obj.push(data);
+            result_text.setText(obj.expression);
+        }
     }
 
 
+
+//        String symbol;
+//        switch (view.getId()) {
+//            case R.id.clear:
+//                CalculatorClass.history = ""; //assign history to blank string ""
+//                result_text.setText(CalculatorClass.history); // result_text displayed set to CalculatorClass.history which is ""
+//                CalculatorClass.historyArrayList.clear();// clear the historyArrayList of strings
+//                break;
+//            case R.id.toAdvancedButton: //if AdvancedButton is clicked case
+//                Intent firstIntent = new Intent(MainActivity.this, AdvancedVersion.class); //Display intent/switch screen to MainActivity.this, pulling from AdvancedVersion.class logic
+//                startActivity(firstIntent); //goes into AdvanceVersion
+//                break;
+//            default: //all other buttons for calculation (without clear button and advnacedbutton
+//                //get user input and validate it
+//                symbol = ((Button) view).getText().toString(); //assign symbol to be Buttonclicked as string
+//                if (CalculatorClass.isNumber(symbol)) { //of symbol (button clicked) is a number
+//                    CalculatorClass.push(symbol); //add the number to the CalculatorClass ArrayList of strings and to history (String)
+//                    result_text.setText(CalculatorClass.history); //display the history Strings in result_text (calc display)
+//                } else {
+//                    if (CalculatorClass.history.isEmpty()) { //if history strings is empty (no numbers to calculate)
+//                        Toast.makeText(MainActivity.this, "First input has to be a number", Toast.LENGTH_SHORT).show(); //throw error/Toast and show to user
+//                    } else {
+//                        String previousSymbol = String.valueOf(calculatorClass.history.charAt(CalculatorClass.history.length() - 1)); //create previousSymbol which is String valueOf history String
+//                        if (CalculatorClass.isOperator(previousSymbol)) { //check is previousSymbol entered as operator again?
+//                            Toast.makeText(MainActivity.this, "You cannot use two operators in a row, enter a number", Toast.LENGTH_SHORT).show(); // throw error if two operators are back to back
+//                        } else {
+//                            CalculatorClass.push(symbol); //add the entered button (symbol) to historyListArray of strings and update history string
+//                            if (symbol.equals("=")) { //if button entered is '='
+//                                if (CalculatorClass.historyArrayList.size() < 3) { //check if historyArrayList has less than 3 elements (num1, operator1, num2) required for a valid compute/calculation
+//                                    Toast.makeText(MainActivity.this, "This cannot be calculated, please enter proper values and operators to calculate.", Toast.LENGTH_SHORT).show(); //if true, throw error
+//                                    CalculatorClass.historyArrayList.clear(); //clear historyArrayList of previously pushed elements into the array
+//                                    calculatorClass.history = CalculatorClass.history.substring(0, CalculatorClass.history.length() - 1); //assign history string to
+//                                    //System.out.print("calculatorClass.history"+calculatorClass.history);
+//                                } else {
+//                                    CalculatorClass.advancedCalculate();
+//                                    result_text.setText(CalculatorClass.history);
+//                                    CalculatorClass.history = "";
+//                                }
+//                            } else {
+//                                result_text.setText(CalculatorClass.history);
+//                            }
+//                        }
+//                    }
+//                }
+
+
+//        };
+//    }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Calculator App" , "Activity Started ");
+        Log.d("Calculator App", "Activity Started ");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Calculator App" , "Activity Resumed ");
+        Log.d("Calculator App", "Activity Resumed ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("Calculator App" , "Activity Paused ");
+        Log.d("Calculator App", "Activity Paused ");
 
     }
 
@@ -177,22 +244,5 @@ public class MainActivity extends AppCompatActivity
         // save some values
         super.onDestroy();
     }
-
-
-//    private void validate(String op){
-//
-//        if (!firstNumberText.getText().toString().isEmpty() &&
-//                !secondNumberText.getText().toString().isEmpty()) {
-//            int fn = Integer.parseInt(firstNumberText.getText().toString());
-//            int sn = Integer.parseInt(secondNumberText.getText().toString());
-//            int result = calculatorClass.calculate(fn, sn, op);
-//            result_text.setText(calculatorClass.history);
-//
-//        else {
-//            Toast.makeText(MainActivity.this,"You have to enter both numbers",Toast.LENGTH_LONG).show();
-//        }
-//
-//    }
-
-
 }
+
